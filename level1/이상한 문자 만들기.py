295858cd808e01edef70a5507725f48eca5e791b
shkim9604 <shkim9604@naver.com>
2023-03-01 23:17:43 +0900

    level 1

---
+++ level1/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    s = s.split(" ")
+    for i in s:
+        for j in range(len(i)):
+            if j % 2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += " "
+    answer = answer[:-1:]
+    return answer
