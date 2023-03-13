1e070e20f0e3b9d3b9a2a6092588402606522620
shkim9604 <shkim9604@naver.com>
2023-03-13 23:17:40 +0900

    level 1

---
+++ level1/과일 장수.py
@@ -0,0 +1,7 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse= True)
+    for i in range(0,len(score),m):
+        if i + m <= len(score):
+            answer += score[i+m-1] * m
+    return answer
